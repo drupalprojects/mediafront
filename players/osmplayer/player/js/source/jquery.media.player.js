3ccc6d1b9097c980e4be6c57269025a0fe35f684
Travis Tidwell <travist349@gmail.com>
2011-05-27 21:27:19 -0500

    Fixed an issue with the player not working with jQuery < 1.3.2

--- players/osmplayer/player/js/source/jquery.media.player.js
+++ players/osmplayer/player/js/source/jquery.media.player.js
@@ -208,8 +208,8 @@
                _this.onNodeLoad( data );
             });
             
-            if( this.node.player ) {
-               this.node.player.display.bind( "mediaupdate", function( event, data ) {
+            if( this.node.player && this.node.player.media ) {
+               this.node.player.media.display.bind( "mediaupdate", function( event, data ) {
                   _this.onMediaUpdate( data );
                });
             }            
