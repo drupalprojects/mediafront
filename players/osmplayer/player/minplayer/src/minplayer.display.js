b134d4b39515eb87b51f270bc109915fa29cfe27
Travis Tidwell <travist349@gmail.com>
2012-05-14 21:14:58 -0700

    Fixed the issue where the remote playlist was not working.

--- players/osmplayer/player/minplayer/src/minplayer.display.js
+++ players/osmplayer/player/minplayer/src/minplayer.display.js
@@ -35,6 +35,15 @@ minplayer.display.prototype.getDisplay = function() {
 };
 
 /**
+ * @see minplayer.plugin.isValid
+ *
+ * @return {boolean} TRUE if the plugin display is valid.
+ */
+minplayer.display.prototype.isValid = function() {
+  return minplayer.plugin.prototype.isValid.call(this) && !!this.display.length;
+};
+
+/**
  * @see minplayer.plugin.construct
  */
 minplayer.display.prototype.construct = function() {
