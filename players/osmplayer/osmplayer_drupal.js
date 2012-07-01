17c85d83d90bea57b3f888701e02f272ceff7126
Travis Tidwell <travist349@gmail.com>
2012-07-01 15:47:37 -0700

    Fixed an issue with the player-to-player where the bindings wouldn't occur for certain situations.

--- players/osmplayer/osmplayer_drupal.js
+++ players/osmplayer/osmplayer_drupal.js
@@ -2,10 +2,25 @@
   $(document).ready(function() {
 
     // Iterate through each mediafront player settings.
+    var plugins = {};
     $.each(Drupal.settings.mediafront, function(id, settings) {
+      if (typeof plugins[settings.preset] !== 'object') {
+        plugins[settings.preset] = {};
+      }
+      plugins[settings.preset][settings.id] = $("#" + id).osmplayer(settings);
+    });
 
-      // Create the osmplayer.
-      $("#" + id).osmplayer(settings);
+    // Now setup all the connections.
+    $.each(Drupal.settings.mediafront_connect, function(plugin_id, settings) {
+      minplayer.get(plugin_id, settings.type, function(plugin) {
+        $.each(settings.connect, function(preset, preset) {
+          $.each(plugins[preset], function(player_id, player) {
+            minplayer.get(player_id, "player", function(player) {
+              player.addPlugin(settings.type, plugin);
+            });
+          });
+        });
+      });
     });
   });
 })(jQuery);
