e4ac35afe0374577cfcd8fd2192a2ca4c9ab2ba8
Travis Tidwell <travist349@gmail.com>
2012-07-12 20:48:12 -0500

    Fixed an issue where the settings not being set could throw an error.

--- players/osmplayer/osmplayer_drupal.js
+++ players/osmplayer/osmplayer_drupal.js
@@ -3,26 +3,30 @@
 
     // Iterate through each mediafront player settings.
     var plugins = {};
-    $.each(Drupal.settings.mediafront, function(id, settings) {
-      if (typeof plugins[settings.preset] !== 'object') {
-        plugins[settings.preset] = {};
-      }
-      plugins[settings.preset][settings.id] = $("#" + id).osmplayer(settings);
-    });
+    if (Drupal.settings.hasOwnProperty('mediafront')) {
+      $.each(Drupal.settings.mediafront, function(id, settings) {
+        if (typeof plugins[settings.preset] !== 'object') {
+          plugins[settings.preset] = {};
+        }
+        plugins[settings.preset][settings.id] = $("#" + id).osmplayer(settings);
+      });
+    }
 
     // Now setup all the connections.
-    $.each(Drupal.settings.mediafront_connect, function(plugin_id, settings) {
-      minplayer.get(plugin_id, settings.type, function(plugin) {
-        $.each(settings.connect, function(preset, preset) {
-          if (plugins[preset]) {
-            $.each(plugins[preset], function(player_id, player) {
-              minplayer.get(player_id, "player", function(player) {
-                player.addPlugin(settings.type, plugin);
+    if (Drupal.settings.hasOwnProperty('mediafront_connect')) {
+      $.each(Drupal.settings.mediafront_connect, function(plugin_id, settings) {
+        minplayer.get(plugin_id, settings.type, function(plugin) {
+          $.each(settings.connect, function(preset, preset) {
+            if (plugins[preset]) {
+              $.each(plugins[preset], function(player_id, player) {
+                minplayer.get(player_id, "player", function(player) {
+                  player.addPlugin(settings.type, plugin);
+                });
               });
-            });
-          }
+            }
+          });
         });
       });
-    });
+    }
   });
 })(jQuery);
