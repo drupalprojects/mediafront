ad6272ffbbf66300543a6c240f9b49e67b2c28b3
Travis Tidwell <travist349@gmail.com>
2013-01-27 23:07:43 +0100

    Fixed the media player so that it can be loaded inside of a modal more easily.

--- players/osmplayer/osmplayer_drupal.js
+++ players/osmplayer/osmplayer_drupal.js
@@ -13,24 +13,26 @@
           });
         });
       }
+
+      // Now setup the mediafront connections.
+      if (Drupal.settings.hasOwnProperty('mediafront_connect')) {
+        $.each(Drupal.settings.mediafront_connect, function(plugin_id, settings) {
+          if (!settings.connected) {
+            settings.connected = true;
+            minplayer.get(plugin_id, settings.type, function(plugin) {
+              $.each(settings.connect, function(preset, preset) {
+                if (plugins[preset]) {
+                  $.each(plugins[preset], function(player_id, player) {
+                    minplayer.get(player_id, "player", function(player) {
+                      player.addPlugin(settings.type, plugin);
+                    });
+                  });
+                }
+              });
+            });
+          }
+        });
+      }
     }
   };
-  
-  $(document).ready(function() {
-    if (Drupal.settings.hasOwnProperty('mediafront_connect')) {
-      $.each(Drupal.settings.mediafront_connect, function(plugin_id, settings) {
-        minplayer.get(plugin_id, settings.type, function(plugin) {
-          $.each(settings.connect, function(preset, preset) {
-            if (plugins[preset]) {
-              $.each(plugins[preset], function(player_id, player) {
-                minplayer.get(player_id, "player", function(player) {
-                  player.addPlugin(settings.type, plugin);
-                });
-              });
-            }
-          });
-        });
-      });
-    }  
-  });
 })(jQuery);
